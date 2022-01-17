a626d42342050bbe8314c923b8f1bd70bdca2384
kiranbansode <kiran5120135@gmail.com>
2022-01-17 14:00:26 +0530

    2.12. More on Strings

--- 02/assets/scripts/app.js
+++ 02/assets/scripts/app.js
@@ -4,6 +4,8 @@ let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 -1";
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+let errorMessage = "An error \n occurred!";
 
 outputResult(currentResult, calculationDescription);
