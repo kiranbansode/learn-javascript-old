dec3dd31384066ba522aff754d22302067e42fd4
kiranbansode <kiran5120135@gmail.com>
2022-01-18 11:27:43 +0530

    2.29. Splitting Code into Functions

--- 02/assets/scripts/app.js
+++ 02/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberIntput() {
+	return parseInt(userInput.value);
+}
+
 function add() {
-	currentResult = currentResult + parseInt(userInput.value);
-	outputResult(currentResult, "");
+	const enteredNumber = getUserNumberIntput();
+	const calcDescription = `${currentResult} + ${enteredNumber}`;
+	currentResult = currentResult + parseInt(enteredNumber);
+	outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
