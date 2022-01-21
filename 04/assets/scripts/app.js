0131bd00bd0eee0cf48669e75667542f16838f43
kiranbansode <kiran5120135@gmail.com>
2022-01-21 14:04:06 +0530

    4.30. The for Loop

--- 04/assets/scripts/app.js
+++ 04/assets/scripts/app.js
@@ -174,7 +174,9 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-	console.log(battleLog);
+	for (let i = 0; i < battleLog.length; i++) {
+		console.log(battleLog[i]);
+	}
 }
 
 attackBtn.addEventListener("click", attackHandler);
