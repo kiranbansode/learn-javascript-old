4436b53d08bd725b5c391b6b9a49d670ac2f216a
kiranbansode <kiran5120135@gmail.com>
2022-01-22 21:03:46 +0530

    5.3. var vs let & const - Introducing Block Scope

---
+++ 05/block-scope/app.js
@@ -0,0 +1,26 @@
+let name = "Kiran";
+
+if (name === "Kiran") {
+	let hobbies = ["Sports", "Cooking"];
+	console.log(hobbies);
+}
+
+{
+	let test = 5;
+	console.log(test);
+}
+
+// test will not work
+console.log(test);
+
+function greet() {
+	let age = 30;
+	let name = "Meera";
+	// hobbies will not work
+	console.log(name, age, hobbies);
+}
+
+// hobbies will not work
+console.log(name, hobbies);
+
+greet();
