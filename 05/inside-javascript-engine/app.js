2b3a288af176cacdd9650a5a1db75fb9d154682f
kiranbansode <kiran5120135@gmail.com>
2022-01-22 23:32:50 +0530

    5.8. Inside the JavaScript Engine - How the Code Executes

---
+++ 05/inside-javascript-engine/app.js
@@ -0,0 +1,25 @@
+/* function getName() {
+	return prompt("Your name :", "");
+}
+
+function greet() {
+	const userName = getName();
+	console.log(`Hello ${userName}`);
+}
+
+greet(); */
+
+const addListenerBtn = document.getElementById("add-listener-btn");
+const clickableBtn = document.getElementById("clickable-btn");
+const messageInput = document.getElementById("click-message-input");
+
+function printMessage() {
+	const value = messageInput.value;
+	console.log(value || "Clicked me!");
+}
+
+function addListener() {
+	clickableBtn.addEventListener("click", printMessage);
+}
+
+addListenerBtn.addEventListener("click", addListener);
