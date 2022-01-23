f8a25a01a71ba89c6577b009f78aef281e34b75a
kiranbansode <kiran5120135@gmail.com>
2022-01-23 22:37:37 +0530

    6.9. Working on the Project Adding User Choices to the Game

--- 06/app.js
+++ 06/app.js
@@ -1,21 +1,31 @@
 const startGameBtn = document.getElementById("start-game-btn");
 
-// function expression
-const start = function () {
-	console.log("Game is starting...");
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+	const selection = prompt(
+		`${ROCK}, ${PAPER} or ${SCISSORS}?`,
+		""
+	).toUpperCase();
+
+	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+		alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+		return DEFAULT_USER_CHOICE;
+	}
+	return selection;
 };
-
-/* const person = {
-	name: "Kiran",
-	greet: function greet() {
-		console.log("Hello there!");
-	},
-};
-
-person.greet();
-
-console.dir(startGame);  */
 
 startGameBtn.addEventListener("click", function () {
-	console.log("Game is already started...", age);
+	if (gameIsRunning) {
+		return;
+	}
+	gameIsRunning = true;
+	console.log("Game is already started...");
+	const playerSelection = getPlayerChoice();
+	console.log(playerSelection);
 });
