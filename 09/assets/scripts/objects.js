a9cdfdab65c82a5b4649ec6e9dd9e1eefdb5575b
kiranbansode <kiran5120135@gmail.com>
2022-03-01 15:39:44 +0530

    9.8. Dynamic Property Access & Setting Properties Dynamically

--- 09/assets/scripts/objects.js
+++ 09/assets/scripts/objects.js
@@ -3,11 +3,15 @@ const movieList = document.getElementById("movie-list");
 movieList.style["background-color"] = "red";
 movieList.style.display = "block";
 
+const userChosenKeys = "level";
+
 // create an object
 const person = {
 	// special keys
 	"first name": "Kiran",
 	age: 26,
+	// setting property dynamically
+	[userChosenKeys]: "...",
 	hobbies: ["Sports", "Coocking"],
 	greet: function () {
 		alert("Hi there");
@@ -28,7 +32,9 @@ delete person.age;
 // Adding new property
 person.isAdmin = true;
 
-// Square Bracket Property Access
-console.log(person["first name"]);
+const keyName = "first name";
+
+// access property dynamically
+console.log(person[keyName]);
 console.log(person[1.5]);
 console.log(person);
