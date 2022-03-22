a0a43bf24b836b09ab44f9a61569aa6ef5bb939f
kiranbansode <kiran5120135@gmail.com>
2022-03-22 16:13:47 +0530

    9.21. The this Keyword And Its Strange Behavior

--- 09/assets/scripts/objects.js
+++ 09/assets/scripts/objects.js
@@ -29,7 +29,9 @@ const renderMovies = (filter = "") => {
 		const { info, ...otherProps } = movie;
 		console.log(otherProps);
 		// const { title: movieTitle } = info;
-		let text = movie.getFormattedTitle() + " - ";
+		let { getFormattedTitle } = movie;
+		getFormattedTitle = getFormattedTitle.bind(movie);
+		let text = getFormattedTitle() + " - ";
 		for (const key in info) {
 			if (key !== "title") {
 				text = text + `${key}: ${info[key]}`;
