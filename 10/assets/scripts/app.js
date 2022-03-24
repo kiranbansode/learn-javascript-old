bb26a64c2bf09060ea2c5eed87e48cf8e78bc39c
kiranbansode <kiran5120135@gmail.com>
2022-03-25 00:03:30 +0530

    10.10. Communicating Can Be Challenging

--- 10/assets/scripts/app.js
+++ 10/assets/scripts/app.js
@@ -16,6 +16,12 @@ class Product {
 
 class ShoppingCart {
 	items = [];
+
+	addProduct(product) {
+		this.items.push(product);
+		this.totalOutput = `<h2>Total : \$${1}</h2>`;
+	}
+
 	render() {
 		const cartEl = document.createElement("section");
 		cartEl.innerHTML = `
@@ -23,6 +29,7 @@ class ShoppingCart {
         <button>Order Now!</button>
         `;
 		cartEl.className = "cart";
+		this.totalOutput = cartEl.querySelector("h2");
 		return cartEl;
 	}
 }
